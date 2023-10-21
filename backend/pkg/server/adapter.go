3ee4b796a0af994d90fe905d3e222488ae90631b
adilahmeddev <adilahmeddev@gmail.com>
2023-10-21 13:52:35 +0100

    vim git is shit

--- backend/pkg/server/adapter.go
+++ backend/pkg/server/adapter.go
@@ -5,26 +5,18 @@ import (
 	"net/http"
 )
 
-type Handler struct{
-
-
-}
-
+type Handler struct{}
 
 func (h *Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	var requestDTO RequestDTO;
+	var requestDTO RequestDTO
 
 	err := json.NewDecoder(req.Body).Decode(&requestDTO)
-
-	if err != nil{
-		res.WriteHeader(http.StatusBadRequest);
-		res.Write([]byte(err.Error()));
-		return;
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte(err.Error()))
+		return
 	}
-
-
 }
-
 
 type RequestDTO struct {
 	Content string `json:"content"`
--- backend/pkg/server/server.go
+++ backend/pkg/server/server.go
@@ -1,5 +1,33 @@
 package server
 
-type Server struct {
+import (
+	"fmt"
+	"net/http"
+)
 
+type Server struct {
+	server *http.Server
+	port   string
+}
+
+func NewServer(port string) *Server {
+	return &Server{
+		server: &http.Server{},
+		port:   port,
+	}
+
+}
+
+func (s *Server) ListenAndServe() error {
+	s.server.Handler = &Handler{}
+	s.server.Addr = ":" + s.port
+	err := s.server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Printf("listening and serving on %q\n", s.server.Addr)
+
+	return nil
 }
