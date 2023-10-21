5d4fbaa2d6b3af08734b44bde73c0703dbecf4b1
unknown <adilahmeddev@gmail.com>
2023-10-21 12:00:18 +0100

    the beginnings of a basic web handler function

--- backend/pkg/server/adapter.go
+++ backend/pkg/server/adapter.go
@@ -1,1 +1,31 @@
-package server 
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Handler struct{
+
+
+}
+
+
+func (h *Handler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	var requestDTO RequestDTO;
+
+	err := json.NewDecoder(req.Body).Decode(&requestDTO)
+
+	if err != nil{
+		res.WriteHeader(http.StatusBadRequest);
+		res.Write([]byte(err.Error()));
+		return;
+	}
+
+
+}
+
+
+type RequestDTO struct {
+	Content string `json:"content"`
+}
